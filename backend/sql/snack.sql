e33d2292620606219ba1c0c38e31f7a7ac770a72
Eduardo Cavalcante de Brito <edwinclarwis777@gmail.com>
2025-10-22 11:42:57 -0300

    att sql
    só coloquei um admin direto no banco para não ter que  criar toda hora

--- backend/sql/snack.sql
+++ backend/sql/snack.sql
@@ -1,81 +1,263 @@
-CREATE DATABASE IF NOT EXISTS snack;
-USE snack;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 22-Out-2025 às 15:38
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.0.30
 
--- Usuários
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    cep VARCHAR(10) ,
-    cidade VARCHAR(50) ,
-    bairro VARCHAR(50),
-    rua VARCHAR(100),
-    numero VARCHAR(10) ,
-    profile_picture VARCHAR(255) DEFAULT NULL,
-    partner BOOLEAN DEFAULT FALSE
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Motoboys
-CREATE TABLE motoboys (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    profile_picture VARCHAR(255) DEFAULT NULL,
-    vehicle_type VARCHAR(50) NOT NULL,
-    license_plate VARCHAR(20) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
--- Administradores
-CREATE TABLE admins (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    profile_picture VARCHAR(255) DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Pedidos
-CREATE TABLE pedidos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT NOT NULL,
-    itens TEXT NOT NULL,
-    endereco TEXT NOT NULL,
-    pagamento VARCHAR(50) NOT NULL,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(20) NOT NULL DEFAULT 'pendente',
-    FOREIGN KEY (usuario_id) REFERENCES users(id)
-);
+--
+-- Banco de dados: `snack`
+--
 
--- Registro de entregas
-CREATE TABLE registro (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    pedido_id INT NOT NULL,
-    cliente_id INT NOT NULL,
-    motoboy_id INT,
-    itens TEXT NOT NULL,
-    endereco TEXT NOT NULL,
-    pagamento VARCHAR(10) NOT NULL,
-    confirmar BOOLEAN DEFAULT FALSE,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
-    FOREIGN KEY (cliente_id) REFERENCES users(id),
-    FOREIGN KEY (motoboy_id) REFERENCES motoboys(id)
-);
+-- --------------------------------------------------------
 
--- Avaliações
-CREATE TABLE reviews (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    pedido_id INT,
-    motoboy_id INT,
-    usuario_id INT NOT NULL,
-    nota INT NOT NULL CHECK (nota BETWEEN 1 AND 5),
-    comentario TEXT,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
-    FOREIGN KEY (motoboy_id) REFERENCES motoboys(id),
-    FOREIGN KEY (usuario_id) REFERENCES users(id)
-);
+--
+-- Estrutura da tabela `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `email`, `senha`, `profile_picture`, `created_at`) VALUES
+(1, 'teste', 'teste@teste', '1234', NULL, '2025-10-22 13:16:06');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `motoboys`
+--
+
+CREATE TABLE `motoboys` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  `vehicle_type` varchar(50) NOT NULL,
+  `license_plate` varchar(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `itens` text NOT NULL,
+  `endereco` text NOT NULL,
+  `pagamento` varchar(50) NOT NULL,
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(20) NOT NULL DEFAULT 'pendente'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `pedidos`
+--
+
+ '2025-10-22 13:33:01', 'pendente');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `registro`
+--
+
+CREATE TABLE `registro` (
+  `id` int(11) NOT NULL,
+  `pedido_id` int(11) NOT NULL,
+  `cliente_id` int(11) NOT NULL,
+  `motoboy_id` int(11) DEFAULT NULL,
+  `itens` text NOT NULL,
+  `endereco` text NOT NULL,
+  `pagamento` varchar(10) NOT NULL,
+  `confirmar` tinyint(1) DEFAULT 0,
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `pedido_id` int(11) DEFAULT NULL,
+  `motoboy_id` int(11) DEFAULT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `nota` int(11) NOT NULL CHECK (`nota` between 1 and 5),
+  `comentario` text DEFAULT NULL,
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `cep` varchar(10) DEFAULT NULL,
+  `cidade` varchar(50) DEFAULT NULL,
+  `bairro` varchar(50) DEFAULT NULL,
+  `rua` varchar(100) DEFAULT NULL,
+  `numero` varchar(10) DEFAULT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  `partner` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Índices para tabela `motoboys`
+--
+ALTER TABLE `motoboys`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Índices para tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Índices para tabela `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pedido_id` (`pedido_id`),
+  ADD KEY `cliente_id` (`cliente_id`),
+  ADD KEY `motoboy_id` (`motoboy_id`);
+
+--
+-- Índices para tabela `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pedido_id` (`pedido_id`),
+  ADD KEY `motoboy_id` (`motoboy_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `motoboys`
+--
+ALTER TABLE `motoboys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `users` (`id`);
+
+--
+-- Limitadores para a tabela `registro`
+--
+ALTER TABLE `registro`
+  ADD CONSTRAINT `registro_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`id`),
+  ADD CONSTRAINT `registro_ibfk_2` FOREIGN KEY (`cliente_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `registro_ibfk_3` FOREIGN KEY (`motoboy_id`) REFERENCES `motoboys` (`id`);
+
+--
+-- Limitadores para a tabela `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`id`),
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`motoboy_id`) REFERENCES `motoboys` (`id`),
+  ADD CONSTRAINT `reviews_ibfk_3` FOREIGN KEY (`usuario_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
